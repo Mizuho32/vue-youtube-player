d44e20d93ddc8d0f4fc4353a65191f9165014399
Mizuho <mizuho128@gmail.com>
2023-07-09 02:37:20 +0900

    modal library

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from "vue";
 import App from "./App.vue";
 import VueYoutube from 'vue-youtube'
+import { vfmPlugin } from 'vue-final-modal'
 
-Vue.use(VueYoutube)
+
+
+Vue.use(VueYoutube);
+Vue.use(vfmPlugin)
 
 Vue.config.productionTip = false;
 
