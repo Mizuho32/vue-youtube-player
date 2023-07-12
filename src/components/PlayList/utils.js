7c006f1222dbf8fbac959fd3c068cb6f0b804cb5
Mizuho <mizuho128@gmail.com>
2023-07-12 23:04:49 +0900

    utils.js loadPlaylist()

--- src/components/PlayList/utils.js
+++ src/components/PlayList/utils.js
@@ -15,6 +15,17 @@ export default {
 					}
 				}, delta);
 			});
-		},
+		}, // waitUntil
+    async loadPlaylist(vm, axios, path, params = {}){
+      const response = await this.axios.get(path, params);
+
+      for (let [k,v] of Object.entries(response.data)) {
+        vm.$set(vm.playList, k, v);
+        for (let album of v) {
+          const ver = album.version;
+          vm.$set(album, "version", ver ? new Date(ver) : undefined);
+        }
+      }
+    },
 	}
 }
